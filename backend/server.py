d2f2b540d236b021a67bc76540237c7a302af3c7
memo <mohammedfouadhassen@gmail.com>
2021-04-06 01:29:33 +0800

    attempt to generate random strings with Python

--- backend/server.py
+++ backend/server.py
@@ -1,18 +1,23 @@
 from flask import Flask, request, jsonify
 import random
+import string
 
 app = Flask(__name__)
 
 # Routes
 @app.route("/", methods=["POST", "GET"])
-def index():
+def get_random_string():
+
+    # With combination of lower and upper case
+    result_str = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(15))
+
     # default Route
-    val = random.random()
     response = {
-        "val": val,
+        "string": result_str,
     }
-    # return name and email as a JSON httpresponse using jsonify
+    # print random string
     return jsonify(response)
+
 # When run from command line, start the server.
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
